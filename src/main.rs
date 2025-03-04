20f49ce30b2c471dcbe004eda0554fde8d624ccd
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-03-04 14:57:25 -0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::env;
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    dotenv().ok();
+
+    let router = Router::new().route("/", get(hello_world));
+
+    Ok(router.into())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
