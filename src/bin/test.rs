2ec43af139ac33abfa26efa123a51bec45c351bd
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-04-19 00:42:46 -0700

    feat: test file and the current pay period method

---
+++ src/bin/test.rs
@@ -0,0 +1,40 @@
+use chrono::{Datelike, Local};
+
+fn main() {
+    let mut current_period = (String::new(), String::new());
+
+    let period_window = (9, 23);
+    let now = Local::now().date_naive();
+    let day = now.day();
+
+    if day <= period_window.0 {
+        current_period.0 = now
+            .with_day(period_window.1 + 1)
+            .unwrap()
+            .with_month(if now.month() == 1 {
+                12
+            } else {
+                now.month() - 1
+            })
+            .unwrap()
+            .to_string();
+        current_period.1 = now.with_day(period_window.0 - 1).unwrap().to_string();
+    } else if day >= period_window.1 {
+        current_period.0 = now.with_day(period_window.1 + 1).unwrap().to_string();
+        current_period.1 = now
+            .with_day(period_window.0 - 1)
+            .unwrap()
+            .with_month(if now.month() == 12 {
+                1
+            } else {
+                now.month() + 1
+            })
+            .unwrap()
+            .to_string();
+    } else {
+        current_period.0 = now.with_day(period_window.0).unwrap().to_string();
+        current_period.1 = now.with_day(period_window.1).unwrap().to_string();
+    }
+
+    println!("{:?}", current_period);
+}
