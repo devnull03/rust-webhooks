8f1f68edd3c6a356129780b9bd988a84f1f49893
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-04-20 01:13:02 -0700

    feat: email with attachment

--- src/email.rs
+++ src/email.rs
@@ -1,9 +1,12 @@
+use chrono::Datelike;
 use resend_rs::{
     types::{CreateEmailBaseOptions, CreateEmailResponse},
     Resend,
 };
 
-pub async fn send_email(
+use crate::notion;
+
+pub async fn _send_email(
     resend: &Resend,
     email_content: &String,
 ) -> Result<CreateEmailResponse, resend_rs::Error> {
@@ -31,12 +34,22 @@ pub async fn _send_notion_webhook_init_email(
 
 pub async fn send_timesheet_email(
     resend: &Resend,
+    timesheet: Vec<u8>,
 ) -> Result<CreateEmailResponse, resend_rs::Error> {
     let from = "devnull03 <dev@dvnl.work>";
     let to = ["arnav.mehta@student.ufv.ca", "arnav@dvnl.work"];
-    let subject = "Notion webhook verification token";
 
-    let email = CreateEmailBaseOptions::new(from, to, subject).with_text("to be implimented");
+    let period = notion::utils::get_current_pay_period();
+
+    let subject = format!(
+        "Timesheet {}/{} to {}/{} - Arnav Mehta",
+        period.0.month(),
+        period.0.day(),
+        period.1.month(),
+        period.1.day()
+    );
+
+    let email = CreateEmailBaseOptions::new(from, to, subject).with_attachment(timesheet);
 
     resend.emails.send(email).await
 }
