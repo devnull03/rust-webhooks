af861f25c46655a871b497dcc96f5405017ccf1e
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-22 00:56:37 -0700

    fix: changed forwarded email to a verified destination

--- cloudflare/job-alert-worker/src/lib.rs
+++ cloudflare/job-alert-worker/src/lib.rs
@@ -8,7 +8,7 @@ async fn main(
     _ctx: Context,
 ) -> Result<()> {
     console_error_panic_hook::set_once();
-    message.forward("dev@dvnl.work".to_string(), None).await?;
+    message.forward("arnav@dvnl.work".to_string(), None).await?;
 
     let client = reqwest::Client::new();
     let _res = client.post("https://hooks.dvnl.work/cloudflare-job-alert-reciever")
