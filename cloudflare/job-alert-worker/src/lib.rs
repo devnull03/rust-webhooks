bb724e10ad877387844c93231ea671d6e46a2312
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-22 21:52:29 -0700

    feat: now sending the email body with the webhook

--- cloudflare/job-alert-worker/src/lib.rs
+++ cloudflare/job-alert-worker/src/lib.rs
@@ -1,21 +1,28 @@
 use worker::*;
+use serde_json::json;
 
 #[event(email)]
 async fn main(message: EmailMessage, _env: Env, _ctx: Context) -> Result<()> {
     console_error_panic_hook::set_once();
-
     console_log!("Received email, forwarding and sending webhook");
-
     // message.forward("arnav@dvnl.work".to_string(), None).await?;
 
     // let webhook_url = "https://hooks.dvnl.work/cloudflare-job-alert-reciever";
     let webhook_url = "https://rust-webhooks-wxht.shuttle.app/cloudflare-job-alert-reciever";
-
     console_log!("Attempting to send webhook to: {}", webhook_url);
+
+    let email_data = json!({
+        "from": message.from_email(),
+        "to": message.to_email(),
+        "raw_content": base64::encode(message.raw_bytes().await.unwrap()),
+        "size": message.raw_size()
+    });
 
     let client = reqwest::Client::new();
     let _res = client
         .post(webhook_url)
+        .header("content-type", "application/json")
+        .json(&email_data)
         .send()
         .await
         .unwrap();
